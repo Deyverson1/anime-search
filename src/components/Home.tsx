861e428fbfe0e295e8874a9dc55c362370aec333
Deyverson1 <deyversongp@gmail.com>
2024-03-07 15:51:58 -0500

    Updates on Header, home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -30,18 +30,21 @@ export default function Home({ setData }: HomeProps) {
   }
   return (
     <>
-      <section className="flex flex-col top-0 w-full bg-cover"
+      <section className="top-0 flex flex-col w-full bg-cover"
       >
-        <header className="w-full flex flex-col pt-8 items-center">
-          <form className="w-8/12 pb-4">
+        <header className="flex flex-col items-center justify-center w-full pt-8 lg:flex-row">
+          <div className="flex items-center justify-center">
+            <img className="w-1/2" src="/images/image.png" alt="" />
+          </div>
+          <form className="w-10/12 pb-4 lg:w-5/12">
             <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
             <div className="relative">
-              <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <div className="absolute inset-y-0 flex items-center pointer-events-none start-0 ps-3">
                 <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                   <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                 </svg>
               </div>
-              <input ref={input} type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Animes..." required />
+              <input ref={input} type="search" id="default-search" className="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg ps-10 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Animes..." required />
               <button onClick={handleClick} type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
             </div>
           </form>
--- src/components/NavDetails.tsx
+++ src/components/NavDetails.tsx
@@ -33,52 +33,55 @@ export default function NavDetails() {
     setValueInput('')
   }
   return (
-    <section className="flex">
-      <nav className="pt-10 pl-8 lg:pl-56 w-fit">
+    <section className="flex flex-col lg:flex-row">
+      <nav className=" w-fit">
         <Link to={'/'}>
-          <HomeIcon />
+          <div className="flex items-center justify-center">
+            <img className="w-1/2" src="/images/image.png" alt="" />
+          </div>
         </Link>
       </nav>
-      <section className="flex flex-col pr-8 top-0 w-full bg-cover justify-center items-center lg:items-stretch "
+      <section className="top-0 flex flex-col items-center justify-center w-full bg-cover lg:items-stretch "
       >
-        <header className=" flex flex-col pt-8 items-center">
-          <form className="w-full lg:w-6/12 pb-4">
+        <header className="flex flex-col items-center ">
+          <form className="w-full pb-4 lg:w-8/12">
             <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
             <div className="relative">
-              <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <div className="absolute inset-y-0 flex items-center pointer-events-none start-0 ps-3">
                 <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                   <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                 </svg>
               </div>
-              <input value={value} onChange={handleChange} ref={inputChange} type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Animes..." required />
+              <input value={value} onChange={handleChange} ref={inputChange} type="search" id="default-search" className="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg ps-10 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Animes..." required />
             </div>
           </form>
-        </header>
-        <section className="absolute z-1 top-24 lg:right-72 justify-end w-7/12 lg:w-5/12">
-         {responseFetch && value != '' && value != undefined && (
-           <main className=" absolute px-2 z-10  bg-neutral-900 lg:right-20 flex-col items-center w-12/12  gap-x-2 py-2 lg:gap-x-4 overflow-y-scroll max-h-56   ">
-           {responseFetch !== null && responseFetch.map((dato: { imageUrl: string, title: string, id: number }, index: number) => (
-             <Link key={index} to={`/anime/${dato.id}`}>
-               <article onClick={handleCleanInput} className="flex over gap-1 flex-1 md:gap-x-10  space-x-0 lg:space-y-4 group md:space-y-0 lg:h-12 max-w-full">
-                 <div className=" flex flex-col  col-span-6 row-span-5 gap-8 transition duration-500 ease-in-out transform shadow-xl overflow-clip rounded-lg sm:rounded-xl md:group-hover:-translate-y-1 md:group-hover:shadow-2xl lg:border lg:border-gray-800 lg:hover:border-gray-700 lg:hover:bg-gray-800/50">
-                   <img
-                     className="object-cover object-top min-w-12 max-w-12 min-h-12 max-h-12 transition duration-500 sm:h-full md:scale-110 md:group-hover:scale-105"
-                     src={dato.imageUrl}
-                     alt=""
-                   />
-                 </div>
-                 <div className="hidden lg:block">
-                   <p className="text-xs font-bold">{dato.title.length < 60 ? dato.title : dato.title.slice(0, 60) + '...'}</p>
-                 </div>
-                 <div className="block lg:hidden">
-                   <p className="text-xs font-bold">{dato.title.length < 30 ? dato.title : dato.title.slice(0, 30) + '...'}</p>
-                 </div>
-               </article>
-             </Link>
-           ))}
-         </main>
-         )}
+          <section className="justify- w-12/12 z-1 lg:right-24 lg:w-8/12">
+          {responseFetch && value != '' && value != undefined && (
+            <main className="z-10 flex-col items-center w-full px-2 py-2 overflow-y-scroll bg-neutral-900 lg:right-20 w-12/12 gap-x-2 lg:gap-x-4 max-h-56">
+              {responseFetch !== null && responseFetch.map((dato: { imageUrl: string, title: string, id: number }, index: number) => (
+                <Link key={index} to={`/anime/${dato.id}`}>
+                  <article onClick={handleCleanInput} className="flex flex-1 max-w-full gap-1 space-x-0 over md:gap-x-10 lg:space-y-4 group md:space-y-0 lg:h-12">
+                    <div className="flex flex-col col-span-6 row-span-5 gap-8 transition duration-500 ease-in-out transform rounded-lg shadow-xl overflow-clip sm:rounded-xl md:group-hover:-translate-y-1 md:group-hover:shadow-2xl lg:border lg:border-gray-800 lg:hover:border-gray-700 lg:hover:bg-gray-800/50">
+                      <img
+                        className="object-cover object-top transition duration-500 min-w-12 max-w-12 min-h-12 max-h-12 sm:h-full md:scale-110 md:group-hover:scale-105"
+                        src={dato.imageUrl}
+                        alt=""
+                      />
+                    </div>
+                    <div className="hidden lg:block">
+                      <p className="text-xs font-bold">{dato.title.length < 60 ? dato.title : dato.title.slice(0, 60) + '...'}</p>
+                    </div>
+                    <div className="block lg:hidden">
+                      <p className="text-xs font-bold">{dato.title.length < 30 ? dato.title : dato.title.slice(0, 30) + '...'}</p>
+                    </div>
+                  </article>
+                </Link>
+              ))}
+            </main>
+          )}
         </section>
+        </header>
+        
       </section>
 
     </section>
