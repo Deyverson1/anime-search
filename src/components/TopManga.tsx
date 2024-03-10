75331739d4b1062ec2a51669258a5593ea45604b
Deyverson1 <deyversongp@gmail.com>
2024-03-09 21:33:43 -0500

    fixed problem with screen load on AnimeResults

--- src/components/TopManga.tsx
+++ src/components/TopManga.tsx
@@ -23,15 +23,15 @@ export default function TopManga() {
   }, [])
   return (
     <section className="flex flex-col w-full px-4 mt-10 lg:mt-14 lg:px-0 gap-y-4">
-      <h1 className="text-xl font-bold text-black">Top Manga:</h1>
+      <h1 className="pl-2 text-xl font-bold text-black">Top Manga:</h1>
       {topManga.map((dato, index) => (
-        <article key={index} className="flex items-center justify-center min-w-80">
+        <article key={index} className="flex items-center justify-center scale-95 cursor-pointer hover:scale-100 min-w-80">
           <h1 className="w-full text-base text-black">{dato.title !== null && dato.title?.length > 25
             ? dato.title.slice(0, 25) + '...'
             : dato.title
           }</h1>
           <div className="flex flex-wrap justify-end w-fit">
-            <p className="px-4 bg-blue-500 rounded-full w-fit hover:scale-110">Manga</p>
+            <p className="px-4 bg-blue-500 rounded-full w-fit ">Manga</p>
           </div>
         </article>
       ))
