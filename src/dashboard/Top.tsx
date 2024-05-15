e9016798ff03a8614dcd9da9a4b092a2fef74ebe
Deyverson1 <deyversongp@gmail.com>
2024-05-14 20:51:40 -0500

    Problems with animechan api

--- src/dashboard/Top.tsx
+++ src/dashboard/Top.tsx
@@ -35,7 +35,7 @@ export default function Top({ data }: TopProps) {
         </div>
         <TopAnimes />
         <Recommend />
-        <Quote />
+        {/* <Quote /> */}
       </section>
     </article>
   )
